90debf61382533ff264d05462aea0aebc97e8f1e
nicmalegre <40902023+nicmalegre@users.noreply.github.com>
2021-08-13 20:51:14 -0300

    Create templates for posts app and create the first one feed.html

--- posts/views.py
+++ posts/views.py
@@ -3,35 +3,37 @@ from django.http import HttpResponse
 from datetime import datetime
 
 
+
 posts = [
     {
-        'name': 'Mont Blac',
-        'user': 'Yesica Cortes',
+        'title': 'Mont Blac',
+        'user': {
+            'name': 'Yesica Cortes',
+            'picture': 'https://picsum.photos/200/200/?image=1027',
+            },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1036'
+        'photo': 'https://picsum.photos/200/200/?image=1036'
     },
     {
-        'name': 'Via Lactea',
-        'user': 'Vander',
+        'title': 'Via Lactea',
+        'user': {
+            'name': 'Vander',
+            'picture': 'https://picsum.photos/200/200/?image=1005',
+            },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=903'
+        'photo': 'https://picsum.photos/200/200/?image=903'
     },
     {
-        'name': 'Nuevo auditorio',
-        'user': 'user',
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'User',
+            'picture': 'https://picsum.photos/200/200/?image=883',
+            },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1076'
+        'photo': 'https://picsum.photos/200/200/?image=1076'
     },
 ]
 
 def list_posts(request):
-    content = []
-
-    for post in posts:
-        content.append("""
-        <p><strong>{name}</strong></p>
-        <p><small>{user} - <i>{timestamp}</i></small></p>
-        <figure><img src="{picture}"></img></figure>
-        """.format(**post))
-
-    return HttpResponse('<br>'.join(content))
+   
+    return render(request, 'feed.html', {'posts': posts})
